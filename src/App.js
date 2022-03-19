c8695e9e2ad5a4e7cfcba17d30eacc241088b388
choijinhap <wlsgkq123@gmail.com>
2022-03-19 17:11:38 +0900

    dev: header & dark mode switch, todo: BottomNavigationAction, page contents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { createTheme, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
+import Layout from './components/Layout';
+
+const lightTheme = createTheme();
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
 function App() {
-	return <div className='App'></div>;
+	const [isThemeLight, setIsThemeLight] = useState(true);
+	return (
+		<ThemeProvider theme={isThemeLight ? lightTheme : darkTheme}>
+			<Layout setIsThemeLight={setIsThemeLight}></Layout>
+		</ThemeProvider>
+	);
 }
 
 export default App;
