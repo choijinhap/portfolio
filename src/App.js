2bfc634e7cd54a8158721b57226965ad7fcd3427
choijinhap <wlsgkq123@gmail.com>
2022-03-28 19:21:24 +0900

    dev: 첫 페이지 Typograpy 애니메이션 생성 , theme에 responsiveFontSizes 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { createTheme, ThemeProvider } from '@mui/material';
-import {  useRef, useState } from 'react';
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material';
+import { useState } from 'react';
 import Layout from './components/Layout';
-
-const lightTheme = createTheme();
-const darkTheme = createTheme({
-	palette: {
-		mode: 'dark',
-	},
-});
+const lightTheme = responsiveFontSizes(createTheme({}));
+const darkTheme = responsiveFontSizes(
+	createTheme(({ theme }) => ({
+		palette: {
+			mode: 'dark',
+		},
+	}))
+);
 function App() {
 	const [isThemeLight, setIsThemeLight] = useState(true);
-	
+
 	return (
 		<ThemeProvider theme={isThemeLight ? lightTheme : darkTheme}>
 			<Layout setIsThemeLight={setIsThemeLight}></Layout>
--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,3 +1,31 @@
+import { Typography } from '@mui/material';
+import { styled } from '@mui/system';
+
+const StyledDiv = styled('div')`
+	animation: fadein 7s ease-in-out;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	height: 100vh;
+	@keyframes fadein {
+		0% {
+			opacity: 0;
+			transform: translateY(20px);
+		}
+		100% {
+			opacity: 1;
+			transform: none;
+		}
+	}
+`;
 export default function Page1() {
-	return <div style={{ height: '100vh' }}>Page1</div>;
+	return (
+		<StyledDiv>
+			<Typography variant='h1'>안녕하세요</Typography>
+			<Typography variant='h1'>Hello</Typography>
+			<Typography variant='h1'>Hello</Typography>
+			<Typography variant='h1'>Hello</Typography>
+		</StyledDiv>
+	);
 }
